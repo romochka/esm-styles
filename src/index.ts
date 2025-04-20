e26943cf98e53ef864c6a913f6bfe4db0c699c0f
Roma Voroneshski <romochka@gmail.com>
2025-04-20 21:55:28 +0300

    reinit

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,6 @@
 // Export types
 export * from './lib/types/index.js'
 
-// Main function export
-export { default as getCss } from './lib/getCss.js'
-
-// Utils exports for advanced usage
-export { joinSelectors, cartesianSelectors } from './lib/utils/selectors.js'
-export { formatContentValue } from './lib/utils/content.js'
-export { jsKeyToCssKey, isEndValue } from './lib/utils/common.js'
-export { processMediaQueries } from './lib/utils/media.js'
-export { obj2css, prettifyCss } from './lib/utils/obj2css.js'
-
 export function greet(): string {
   return 'esm-styles 0.1.3'
 }
