01cd8bd3f264dda567b3bb44d9649a05d793a1f2
Roma Voroneshski <romochka@gmail.com>
2025-04-27 10:40:23 +0300

    completed

--- src/lib/utils/format.ts
+++ src/lib/utils/format.ts
@@ -1,12 +1,19 @@
 // CSS formatting utility for getCss
+import beautify from 'js-beautify'
+
 import type { PrettifyCssString } from '../types/index.js'
 
-export const prettifyCssString: PrettifyCssString = (css) => {
+export const prettifyCssString: PrettifyCssString = (cssString) => {
   // Simple pretty-print: trim and collapse extra whitespace
-  return css
-    .replace(/\s+/g, ' ')
-    .replace(/\s*{\s*/g, ' {\n  ')
-    .replace(/;\s*/g, ';\n  ')
-    .replace(/}\s*/g, '\n}\n')
-    .trim()
+  // return css
+  //   .replace(/\s+/g, ' ')
+  //   .replace(/\s*{\s*/g, ' {\n  ')
+  //   .replace(/;\s*/g, ';\n  ')
+  //   .replace(/}\s*/g, '\n}\n')
+  //   .trim()
+
+  return beautify.css(cssString, {
+    indent_size: 2,
+    end_with_newline: true,
+  })
 }
